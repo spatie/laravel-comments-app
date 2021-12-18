21933651b7ce55f9c77e8b7c31bb8566f085d03a
freek <freek@spatie.be>
2021-12-19 00:33:37 +0100

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 require('./bootstrap');
+
+
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+
+window.setupEditor = function (content) {
+    return {
+        editor: null,
+        content: content,
+
+        init(element) {
+            this.editor = new Editor({
+                element: element,
+                extensions: [
+                    StarterKit,
+                ],
+                content: this.content,
+                onUpdate: ({editor}) => {
+                    this.content = editor.getHTML()
+                }
+            })
+        }
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Test app</title>
+    <script src="{{ mix('js/app.js') }}" defer="defer"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 
     @livewireStyles
