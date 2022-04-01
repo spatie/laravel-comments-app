1d06aca20e141d4a482b63e2fe6ffa4117dd5caf
Freek Van der Herten <freek@spatie.be>
2022-04-01 13:20:50 +0200

    wip

--- database/migrations/2021_12_16_111923_create_comments_tables.php
+++ database/migrations/2021_12_16_111923_create_comments_tables.php
@@ -10,27 +10,36 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->morphs('commentator');
+            $this->nullableMorphs($table, 'commentator', 'commentator_comments');
             $table->morphs('commentable');
             $table->foreignId('parent_id')->nullable()->constrained('comments')->onDelete('cascade');
             $table->longText('original_text');
             $table->longText('text');
             $table->json('extra')->nullable();
+            $table->timestamp('approved_at')->nullable();
             $table->timestamps();
         });
 
         Schema::create('reactions', function (Blueprint $table) {
             $table->id();
-            $table->morphs('commentator');
+            $this->nullableMorphs($table, 'commentator', 'commentator_reactions');
             $table->foreignId('comment_id')->references('id')->on('comments')->cascadeOnDelete();
             $table->string('reaction')->collation('utf8mb4_bin');
             $table->timestamps();
         });
 
         Schema::create('comment_notification_opt_outs', function(Blueprint $table) {
-            $table->morphs('commentator', 'commentator_opt_outs');
-            $table->morphs('commentable', 'opt_outs');
+            $this->nullableMorphs($table, 'commentator', 'commentator_opt_outs');
+            $this->nullableMorphs($table, 'commentable', 'opt_outs');
+
             $table->timestamps();
         });
+    }
+
+    protected function nullableMorphs(Blueprint $table, string $name, string $indexName): void
+    {
+        $table->string("{$name}_type")->nullable();
+        $table->unsignedBigInteger("{$name}_id")->nullable();
+        $table->index(["{$name}_type", "{$name}_id"], $indexName);
     }
 };
