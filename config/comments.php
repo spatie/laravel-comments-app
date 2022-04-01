88d0160a7b5436ddfd3ed3fe554999d8bb51d4f4
Freek Van der Herten <freek@spatie.be>
2022-04-01 11:58:05 +0200

    wip

--- config/comments.php
+++ config/comments.php
@@ -1,7 +1,16 @@
 <?php
 
-
-use Spatie\Comments\Notifications\NewCommentNotification;
+use Spatie\Comments\Notifications\ApprovedCommentNotification;
+use Spatie\Comments\Notifications\PendingCommentNotification;
+use Spatie\Comments\Actions\SendNotificationsForApprovedCommentAction;
+use Spatie\Comments\Actions\RejectCommentAction;
+use Spatie\Comments\Actions\ApproveCommentAction;
+use Spatie\Comments\Actions\SendNotificationsForPendingCommentAction;
+use Spatie\Comments\Actions\ProcessCommentAction;
+use Spatie\Comments\Models\CommentNotificationOptOut;
+use Spatie\Comments\Models\Reaction;
+use Spatie\Comments\Models\Comment;
+use Spatie\Comments\CommentTransformers\MarkdownToHtmlTransformer;
 
 return [
     /*
@@ -10,47 +19,63 @@ return [
      */
     'allowed_reactions' => ['👍', '🥳', '👀', '😍', '💅'],
 
+    'allow_anonymous_comments' => false,
+
     /*
      * A comment transformer is a class that will transform the comment text
      * for example from Markdown to HTML
      */
     'comment_transformers' => [
-        Spatie\Comments\CommentTransformers\MarkdownToHtmlTransformer::class,
+        MarkdownToHtmlTransformer::class,
     ],
 
+    /*
+     * Comments need to be approved before they are shown. You can opt
+     * to have all comments to be approved automatically.
+     */
+    'automatically_approve_all_comments' => true,
+
     'models' => [
-        /*
-         * The model you want to use as a Comment model. It needs to be or
-         * extend the `Spatie\Comments\Models\Comment::class` model.
-         */
-        'comment' => Spatie\Comments\Models\Comment::class,
-
-        /*
-         * The model you want to use as a React model. It needs to be or
-         * extend the `Spatie\Comments\Models\Reaction::class` model.
-         */
-        'reaction' => Spatie\Comments\Models\Reaction::class,
-
-        /*
-         * The model you want to use as an opt-out model. It needs to be or
-         * extend the `Spatie\Comments\Models\CommentNotificationOptOut::class` model.
-         */
-        'comment_notification_opt_out' => Spatie\Comments\Models\CommentNotificationOptOut::class,
-
         /*
          * The class that will comment on other things. Typically, this
          * would be a user model.
          */
         'commentator' => null,
+
+        /*
+         * The model you want to use as a Comment model. It needs to be or
+         * extend the `Spatie\Comments\Models\Comment::class` model.
+         */
+        'comment' => Comment::class,
+
+        /*
+         * The model you want to use as a React model. It needs to be or
+         * extend the `Spatie\Comments\Models\Reaction::class` model.
+         */
+        'reaction' => Reaction::class,
+
+        /*
+         * The model you want to use as an opt-out model. It needs to be or
+         * extend the `Spatie\Comments\Models\CommentNotificationOptOut::class` model.
+         */
+        'comment_notification_opt_out' => CommentNotificationOptOut::class,
     ],
 
     'notifications' => [
+        /*
+         * When somebody creates a comment, a notification will be sent to other persons
+         * that commented on the same thing.
+         */
         'enabled' => true,
 
         'notifications' => [
-            'new_comment' => NewCommentNotification::class,
+            'pending_comment' => PendingCommentNotification::class,
+            'approved_comment' => ApprovedCommentNotification::class,
         ],
 
+        /*
+         * Here you can configure the notifications that will be sent via mail.
+         */
         'mail' => [
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
@@ -59,19 +84,16 @@ return [
         ],
     ],
 
+    /*
+     * Unless you need fine-grained customisation, you don't need to change
+     * these action classes. If you do change any of them, make sure that your class
+     * extends the original action class.
+     */
     'actions' => [
-        /*
-         * This class is responsible for storing the input of the user as a comment.
-         *
-         * Unless you need fine-grained customisation, you don't need to change
-         * this class. If you do change it, make sure that your class
-         * extends `Spatie\Comments\Actions\ProcessCommentAction`.
-         */
-        'process_comment' => Spatie\Comments\Actions\ProcessCommentAction::class,
-
-        /*
-         * This class is responsible for send out notifications for new comments.
-         */
-        'send_notifications_for_new_comment' => Spatie\Comments\Actions\SendNotificationsForNewCommentAction::class,
+        'process_comment' => ProcessCommentAction::class,
+        'send_notifications_for_pending_comment' => SendNotificationsForPendingCommentAction::class,
+        'approve_comment' => ApproveCommentAction::class,
+        'reject_comment' => RejectCommentAction::class,
+        'send_notifications_for_approved_comment' => SendNotificationsForApprovedCommentAction::class,
     ],
 ];
